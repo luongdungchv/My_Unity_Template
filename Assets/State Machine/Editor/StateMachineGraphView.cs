50cd3a6089520363f1366e11d7aca46c8690d645
Dung Luong <50980440+luongdungchv@users.noreply.github.com>
2024-03-27 13:41:37 +0700

    temp

--- Assets/State Machine/Editor/StateMachineGraphView.cs
+++ Assets/State Machine/Editor/StateMachineGraphView.cs
@@ -2,8 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
 
 public class StateMachineGraphView : GraphView
 {
-    
+    private string styleSheetName = "GraphViewStyleSheet";
+    public StateMachineWindow window;
+
+    public StateMachineGraphView(StateMachineWindow window){
+        this.window = window;
+        styleSheets.Add(Resources.Load<StyleSheet>(styleSheetName));
+
+        SetupZoom(ContentZoomer.DefaultMinScale, ContentZoomer.DefaultMaxScale);
+        this.AddManipulator(new ContentDragger());
+        this.AddManipulator(new SelectionDragger());
+        this.AddManipulator(new RectangleSelector());
+        this.AddManipulator(new FreehandSelector());
+
+        GridBackground grid = new GridBackground();
+        Insert(0, grid);
+        grid.StretchToParentSize();
+        //AddSearchWindow();
+    }
+
+    public void CreateNode(){
+        var node = new BaseNode();
+        
+    }
 }
